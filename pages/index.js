ae9cfbb086d86d2c5067bbbba008aa749e351b5b
lacasettadienrica <lacasettadienrica@gmail.com>
2025-07-19 23:45:50 +0200

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Benvenuti alla Casetta di Enrica</h1>
+      <p>La vostra guida digitale è in arrivo!</p>
+    </div>
+  );
+}
